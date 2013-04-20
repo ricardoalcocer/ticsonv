d0148518942c697a733a610d1ce5316c7588047c
Ricardo Alcocer <alco@ricardoalcocer.com>
2013-04-20 12:42:55 -0700

    Initial commit

---
+++ app/README.md
@@ -0,0 +1,7 @@
+tiCSONV
+========================================
+
+tiCSONV is a version of [https://github.com/archan937/csonv.js](CSONV) modified to work with Appcelerator Titanium.
+
+CSONV is a JavaScript library that takes a CSV file and converts it into JSON.  Supports nesting of relational data.
+
---
+++ app/controllers/index.js
@@ -0,0 +1,15 @@
+function doClick(e) {  
+    alert($.label.text);
+}
+
+Ti.include('cycle.js');
+Ti.include('csonv.js');
+
+var csv='family.csv';
+var data = csv.toObjects();
+var indention = "    ";
+var result = JSON.stringify(JSON.decycle(data), null, indention.length);
+
+console.log(result);
+
+$.index.open();
